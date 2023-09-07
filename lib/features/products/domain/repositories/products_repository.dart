46394e352fbbf10ad297d68ec6ef17cac218776e
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-09-07 15:44:07 -0600

    Pequeños cambios en DOMAIN Products

--- lib/features/products/domain/repositories/products_repository.dart
+++ lib/features/products/domain/repositories/products_repository.dart
@@ -1,6 +1,6 @@
 import '../entities/product.dart';
 
-abstract class ProductRepository {
+abstract class ProductsRepository {
   Future<List<Product>> getProductsByPage({int limit = 10, offset = 0});
   Future<Product> getProductById(String id);
   Future<Product> createUpdateProduct(Map<String, dynamic> productLike);
