a6292579bccabcf152b272170f12a3cfafeb7bbb
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-09-07 13:32:39 -0600

    Agregando archivos de DOMAIN  Products

---
+++ lib/features/products/domain/datasources/products_datasource.dart
@@ -0,0 +1,7 @@
+import '../entities/product.dart';
+
+abstract class ProductDatasource {
+  Future<List<Product>> getProductsByPage({int limit = 10, offset = 0});
+  Future<Product> getProductById(String id);
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike);
+}
---
+++ lib/features/products/domain/domain.dart
@@ -0,0 +1,6 @@
+//DATASOURCES
+export './datasources/products_datasource.dart';
+//ENTITIES
+export './entities/product.dart';
+//REPOSITORIES
+export './repositories/products_repository.dart';
